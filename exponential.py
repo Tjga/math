aa564b2e356f8c7b2ef5458fed3a3f3d3b67c079
Gerardo Ruiz <38825370+Tjga@users.noreply.github.com>
2023-02-05 08:33:35 -0800

    Add files via upload

---
+++ exponential.py
@@ -0,0 +1,38 @@
+"""
+code my method to find exponnetial funtion out of coordinates
+
+show image of the exponential function and it passing through the 
+given coordinates and plotting them
+
+b = (y2/y1)**(1/(x2-x1))
+a = (y1/b**x1)
+
+the title, = the equation  y = ab**x
+"""
+
+from pylab import plot, show, legend, title, xlabel, ylabel
+import numpy as np
+
+def program():
+    b = int(round((y2/y1)**(1/(x2-x1))))
+    a = int(round((y1/b**x1)))
+
+    x = np.linspace(x1+10,x1-10, endpoint = True)
+    y = (a * np.exp(b*x)) 
+
+    plot(x,y)
+    plot(x1,y1,'r*')
+    plot(x2,y2,'r*')
+    legend([[x1,y1],[x2,y2]])
+    title("y = {0}*{1}^x".format(a,b))
+    show()
+
+if __name__ == '__main__':
+    x1 = int(input("Enter your x1 value: "))
+    y1 = int(input("Enter your y1 value: "))
+    x2 = int(input("Enter your x2 value: "))
+    y2 = int(input("Enter your y2 value: "))
+
+    program()
+
+
